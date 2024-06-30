3a094540d1bbc661eb74f5f0209a00f9de0b4a15
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-06-30 19:07:55 +0100

    Added a basic structure for testing purposes

--- components/CheckOut.tsx
+++ components/CheckOut.tsx
@@ -1,7 +1,22 @@
+import { StyleSheet, View } from "react-native";
+
 const CheckOut=(items)=>{
 return(
     <>
+    <View style={style.container}>
+
+    </View>
     </>
 )
 }
+const style=StyleSheet.create({
+    container:{
+        flex:1,
+        width:"90%",
+        padding:100,
+        borderWidth:1,
+        borderColor:"#000eee",
+        backgroundColor:"#eee"
+    }
+})
 export default CheckOut;
