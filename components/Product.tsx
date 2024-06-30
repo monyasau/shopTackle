f6dcf0fa6c86a8ec7c64d1de9ff2c6a25fefa4cf
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-06-30 20:32:12 +0100

    Improved styling and dynamism

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,15 +1,37 @@
-import { Image, Text, View } from "react-native"
+import { Image, StyleSheet, Text, View } from "react-native"
 
-const Product=(name,id,image)=>{
+const Product=({name,id,image})=>{
     return(
-        <View>
-            <Image source={require("../assets/favicon.png")}/>
-            <Text>
-                Product name
-            </Text>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 text-white/70 hover:text-white">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-              </svg>
+        <View style={style.container}>
+            <Image style={style.image}  source={image}/>
+            <View style={style.projectInfo}>
+
+            <Text>{name}</Text>
+            <Text>{id}</Text>
+            </View>
         </View>
     )
 }
+const style=StyleSheet.create({
+    container:{
+        borderTopLeftRadius:10,
+        borderTopRightRadius:10,
+        borderWidth:1,
+        borderColor:"#000",
+        width:"45%",
+    },
+    image:{
+        borderTopLeftRadius:10,
+        borderTopRightRadius:10,
+        width:"100%",
+        marginBottom:2,
+        height:120,
+    },
+    projectInfo:{
+        borderColor:"#000",
+        borderTopWidth:1,
+        flexDirection:"row",
+        justifyContent:"space-between"
+    }
+})
+export default Product;
