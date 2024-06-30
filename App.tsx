e2a9c4cf6a7e619c67d9e4f4de73e9c287b0837d
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-06-30 19:06:52 +0100

    Implemented navigation

--- App.tsx
+++ App.tsx
@@ -2,12 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Products from './components/Products';
 import CheckOut from './components/CheckOut';
+import BottomNavigation from './components/BottomNavigation';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [activeWindow,setActiveWindow]=useState("products");
   return (
     <View style={styles.container}>
-      <Products/> 
-      <CheckOut/>
+      {activeWindow=="products"?
+        <Products/>:
+      <CheckOut/>}
+      <BottomNavigation active={activeWindow} setActiveWindow={setActiveWindow}/>
       <StatusBar style="auto" />
     </View>
   );
