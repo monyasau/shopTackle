c9634f7fdd57638a69a85e4b69391ef898091eb0
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-06-30 20:31:31 +0100

    Added dummy ui

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,20 +1,30 @@
 import { StyleSheet, Text, View } from "react-native";
+import Product from "./Product";
 
-const Products =()=>{
+const Products = () => {
     return (
         <>
-        <View style={styles.productsContainer}>
             <Text>Available products</Text>
-        </View>
+            
+            <View style={styles.productsContainer}>
+            <Product name={"Gucci belt"} id={0} image={require("../assets/favicon.png")}/>
+            <Product name={"Gucci belt"} id={0} image={require("../assets/favicon.png")}/>
+            <Product name={"Gucci belt"} id={0} image={require("../assets/favicon.png")}/>
+            <Product name={"Gucci belt"} id={0} image={require("../assets/favicon.png")}/>
+            </View>
         </>
     )
 }
 const styles = StyleSheet.create({
     productsContainer: {
-        flex:1,
-        width:"90%",
-        padding:100,
-        backgroundColor:"#eee"
+        justifyContent:"space-evenly",
+        gap:10,
+        flex: 1,
+        // width: "90%",
+        padding: 10,
+        flexDirection:"row",
+        flexWrap:"wrap",
+        backgroundColor: "#eee"
     }
 })
 export default Products;
