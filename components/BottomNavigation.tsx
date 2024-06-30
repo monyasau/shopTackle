d2263bfafad08ab0914c32a48e4208eb7cca9075
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-06-30 19:07:27 +0100

    Implemented navigation and bottom bar design

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -1,13 +1,44 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
-const BottomNavigation=(active)=>{
-return(
-<>
-<View>
-    <Text></Text>
-    <Text></Text>
-</View>
-</>
-)
+const BottomNavigation = ({active,setActiveWindow}) => {
+    return (
+        <>
+            <View style={style.container}>
+                <TouchableOpacity style={style.pages} onPress={()=>setActiveWindow("products")}>
+                    <Text style={active=="products"?style.active:style.inActive}>Products</Text>
+                    </TouchableOpacity>
+                <TouchableOpacity style={style.pages} onPress={()=>setActiveWindow("checkout")}>
+                    <Text style={active=="checkout"?style.active:style.inActive}>Check Out</Text>
+                    </TouchableOpacity>
+            </View>
+        </>
+    )
 }
+const style = StyleSheet.create({
+    container: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        borderColor: "#000",
+        borderWidth: 1,
+        width: "100%",
+        padding: 10,
+        backgroundColor: "#eee"
+    },
+    pages: {
+        width: "48%",
+        padding: 20,
+        borderColor: "#000",
+        borderWidth: 1,
+        backgroundColor: "#ddd",
+    },
+    active:{
+        color:"#000",
+        textAlign:"center"
+    },
+    inActive:{
+        textAlign:"center",
+        color:"#666"
+    }
+})
 export default BottomNavigation;
