ba75a22d11657d6e4a352e0fb0d1050935f08708
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-01 09:02:15 +0100

    Improved UI design

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,13 +1,14 @@
-import { Image, StyleSheet, Text, View } from "react-native"
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
 const Product=({name,id,image})=>{
     return(
         <View style={style.container}>
-            <Image style={style.image}  source={image}/>
+            <TouchableOpacity style={{position:"absolute",top:0}}>
+                <Text>Add to cart</Text>
+            </TouchableOpacity>
+            <Image style={style.image} alt={name} resizeMode="cover" source={image}/>
             <View style={style.projectInfo}>
-
-            <Text>{name}</Text>
-            <Text>{id}</Text>
+            <Text style={{color:"#6a6a6a",textAlign:"center"}}>{name}</Text>
             </View>
         </View>
     )
@@ -20,7 +21,9 @@ const style=StyleSheet.create({
         borderBottomRightRadius:2,
         borderWidth:1,
         borderColor:"#000",
-        width:"45%",
+        width:130,
+        padding:1
+        
     },
     image:{
         borderTopLeftRadius:9,
@@ -29,11 +32,11 @@ const style=StyleSheet.create({
         height:120,
     },
     projectInfo:{
+        padding:2,
         backgroundColor:"#efefef",
         borderColor:"#000",
         borderTopWidth:1,
-        flexDirection:"row",
-        justifyContent:"space-between"
+        
     }
 })
 export default Product;
