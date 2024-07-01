58c496484c29ac56b669134cd0fd78af349f40d5
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-01 09:03:16 +0100

    Imported images manually

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,33 +1,50 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import Product from "./Product";
+
+// i Imported images manually due to non support of dynamic import of javascript
+import gucciBag from "../assets/productImages/gucciBag.jpg";
+import iphone13pro from "../assets/productImages/iphone13pro.jpg";
+import LG43LK5000PTA from "../assets/productImages/LG43LK5000PTA.jpg";
+import louisVuitonshoe from "../assets/productImages/louisVuitonshoe.jpg";
+import macbookair from "../assets/productImages/macbookair.jpg";
+import macbookpro from "../assets/productImages/macbookpro.jpg";
+import nikeShoe from "../assets/productImages/nikeShoe.jpg";
+import ps5 from "../assets/productImages/ps5.jpg";
+import rolexWatch from "../assets/productImages/rolexWatch.jpg";
+import RoyalBlender from "../assets/productImages/RoyalBlender.jpg";
+import samsungS24Ultra from "../assets/productImages/samsungS24Ultra.jpg";
+import sandiskSSd from "../assets/productImages/sandiskSSd.jpg";
+import surfacepro7 from "../assets/productImages/surfacepro7.jpg";
+
 const productsData = [
-    { id: 0, src: "../assets/productImages/gucciBag.jpg", name: "Gucci Bag" },
-    { id: 1, src: "../assets/productImages/iphone13pro.jpg", name: "Iphone 13 pro" },
-    { id: 2, src: "../assets/productImages/LG43LK5000PTA.jpg", name: "LG 43LK5000PTA TV" },
-    { id: 3, src: "../assets/productImages/louisVuitonshoe.jpg", name: "Louis Vuiton Shoe" },
-    { id: 4, src: "../assets/productImages/macbookair.jpg", name: "Mac book Air" },
-    { id: 5, src: "../assets/productImages/macbookpro.jpg", name: "Mac book Pro" },
-    { id: 6, src: "../assets/productImages/nikeShoe.jpg", name: "Nike Shoe" },
-    { id: 7, src: "../assets/productImages/ps5.jpg", name: "Playstation 5(PS5)" },
-    { id: 8, src: "../assets/productImages/rolexWatch.jpg", name: "Rolex Watch" },
-    { id: 9, src: "../assets/productImages/RoyalBlender.jpg", name: "Royal Blender" },
-    { id: 10, src: "../assets/productImages/samsungS24Ultra.jpg", name: "Samsung S24 Ultra" },
-    { id: 11, src: "../assets/productImages/sandiskSSd.jpg", name: "1 TB Sandisk SSD" },
-    { id: 12, src: "../assets/productImages/surfacepro7.jpg", name: "Surface Pro 7" },
+    { id: 0, src: gucciBag, name: "Gucci Bag" },
+    { id: 1, src: iphone13pro, name: "Iphone 13 pro" },
+    { id: 2, src: LG43LK5000PTA, name: "LG 43LK50PTA TV" },
+    { id: 3, src: louisVuitonshoe, name: "Louis Vuiton Shoe" },
+    { id: 4, src: macbookair, name: "Mac book Air" },
+    { id: 5, src: macbookpro, name: "Mac book Pro" },
+    { id: 6, src: nikeShoe, name: "Nike Shoe" },
+    { id: 7, src: ps5, name: "Playstation 5(PS5)" },
+    { id: 8, src: rolexWatch, name: "Rolex Watch" },
+    { id: 9, src: RoyalBlender, name: "Royal Blender" },
+    { id: 10, src: samsungS24Ultra, name: "Samsung S24 Ultra" },
+    { id: 11, src: sandiskSSd, name: "1 TB Sandisk SSD" },
+    { id: 12, src: surfacepro7, name: "Surface Pro 7" },
 ]
 
 const Products = () => {
     return (
         <>
-            <Text>Available products</Text>
 
+            <Text style={{textAlign:"center"}}>Available products</Text>
             <ScrollView contentContainerStyle={styles.productsContainer}>
                 {productsData.map((item)=>{
                     return (
-                        <Text>jje</Text>
+                        <View key={item.id}>
+                            <Product image={item.src} name={item.name} id={item.id}/>
+                        </View>
                     )
                 })}
-                {/* <Product name={"Gucci belt"} id={0} image={require("../assets/testitem.jpg")} /> */}
             </ScrollView>
         </>
     )
@@ -40,7 +57,6 @@ const styles = StyleSheet.create({
         padding: 10,
         flexDirection: "row",
         flexWrap: "wrap",
-        backgroundColor: "#eee"
     }
 })
 export default Products;
